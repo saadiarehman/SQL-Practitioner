5ca00337a3c52d2425f6a7b3430cbdb45a24c63c
Saadia <saadiarehman25@gmail.com>
2020-07-11 16:59:53 +0500

    add bookstore

---
+++ bookstore.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2020 at 01:45 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bookstore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors`
+--
+
+CREATE TABLE `authors` (
+  `author_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `authors`
+--
+
+INSERT INTO `authors` (`author_id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'asad', '2020-04-22 16:37:49', '2020-07-01 16:37:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `author_book`
+--
+
+CREATE TABLE `author_book` (
+  `author_id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `publication_year` varchar(100) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `books`
+--
+
+INSERT INTO `books` (`book_id`, `title`, `description`, `publication_year`, `created_at`, `updated_at`) VALUES
+(12, 'asdaaa', 'qwea', '2007', '2020-07-23 16:39:52', '2020-07-13 16:39:52');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`author_id`);
+
+--
+-- Indexes for table `author_book`
+--
+ALTER TABLE `author_book`
+  ADD KEY `foreignkey` (`author_id`,`book_id`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ commands.sql
@@ -0,0 +1,11 @@
+mysql -u root /---- to open sql----/
+show databases; /----show existing dbs-----/
+create databases x /---- use to create db of name x-----/
+use x /-----to go into db name x ------/
+show tables /----- to show tables in db ------/
+create table x (value datatype PK AUTO_INCREMENT NOT NULL,value datatype,........) /-----table x created---------/
+describe tables /----to show complete in table---->
+drop table x /-------to delete table ----------/
+insert into user (x,y,z) values(x,y,z)
+select * from x;
+SELECT * FROM `user` WHERE  id=1';
