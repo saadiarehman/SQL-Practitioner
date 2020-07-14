1f9201d5712aa25bad54997381e0131d9f1d6cdb
Saadia <saadiarehman25@gmail.com>
2020-07-14 14:04:45 +0500

    fetch data from db as array and display in string

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ require 'task.php';
 
 try{
     $pdo =new PDO('mysql:host=127.0.0.1;dbname=bookstore','root','');
-    $sql='SELECT distinct books.title,AUTHORS.name FROM author_book JOIN AUTHORS on author_book.author_id = authors.author_id JOIN books on author_book.book_id = books.book_id ';
+    $sql='SELECT books.title,GROUP_CONCAT(DISTINCT(AUTHORS.name))AS Authors FROM author_book JOIN AUTHORS on author_book.author_id = authors.author_id JOIN books on author_book.book_id = books.book_id GROUP BY (books.title) 
+';
     $result=$pdo->query($sql);
     $result->setFetchMode(PDO::FETCH_ASSOC);
 
@@ -25,9 +26,9 @@ try{
             <h1>Author Name with Book Title</h1>
             
             <?php while ($row = $result->fetch()): ?>
-                            <?php echo $row['title']  ?>
-                            ---
-                            <?php echo $row['name'] ?>
+                            <?php echo implode("  --- ",$row)  ?>
+                          
+                            
                            <br>
                             
                                                     
