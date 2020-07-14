4c6f7fe0aa321d3dee599889febcff4738f51815
Saadia <saadiarehman25@gmail.com>
2020-07-14 14:29:43 +0500

    fetch data from db as array and display in string

--- bookstore.sql
+++ bookstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2020 at 01:45 PM
+-- Generation Time: Jul 14, 2020 at 11:27 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.31
 
@@ -29,9 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `authors` (
   `author_id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+  `name` varchar(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -39,7 +39,9 @@ CREATE TABLE `authors` (
 --
 
 INSERT INTO `authors` (`author_id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'asad', '2020-04-22 16:37:49', '2020-07-01 16:37:49');
+(1, 'hassan', '2018-07-01 07:23:51', '2019-07-31 07:23:51'),
+(2, 'hamza', '2018-05-19 07:23:51', '2019-06-24 07:23:51'),
+(3, 'ali', '2001-07-05 10:11:02', '2019-07-05 10:11:02');
 
 -- --------------------------------------------------------
 
@@ -48,9 +50,24 @@ INSERT INTO `authors` (`author_id`, `name`, `created_at`, `updated_at`) VALUES
 --
 
 CREATE TABLE `author_book` (
-  `author_id` int(11) NOT NULL,
-  `book_id` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `author_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `author_book`
+--
+
+INSERT INTO `author_book` (`id`, `book_id`, `author_id`) VALUES
+(10001, 101, 1),
+(10004, 101, 1),
+(10003, 101, 2),
+(10005, 101, 3),
+(10002, 102, 2),
+(10006, 103, 3),
+(10007, 104, 1),
+(10008, 104, 2);
 
 -- --------------------------------------------------------
 
@@ -59,20 +76,23 @@ CREATE TABLE `author_book` (
 --
 
 CREATE TABLE `books` (
-  `book_id` int(11) NOT NULL,
+  `book_id` int(100) NOT NULL,
   `title` varchar(100) NOT NULL,
   `description` varchar(100) NOT NULL,
-  `publication_year` varchar(100) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
+  `publication_year` int(100) NOT NULL,
+  `bookcreated_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `bookupdated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`book_id`, `title`, `description`, `publication_year`, `created_at`, `updated_at`) VALUES
-(12, 'asdaaa', 'qwea', '2007', '2020-07-23 16:39:52', '2020-07-13 16:39:52');
+INSERT INTO `books` (`book_id`, `title`, `description`, `publication_year`, `bookcreated_at`, `bookupdated_at`) VALUES
+(101, 'science', 'general science', 2002, '2019-02-09 07:25:31', '2019-07-05 07:25:31'),
+(102, 'geology', 'about earth', 2014, '2014-10-14 07:25:31', '2018-07-10 07:25:31'),
+(103, 'maths', 'mathematics', 2006, '2000-07-26 10:12:14', '2020-07-12 10:12:14'),
+(104, 'islamiyat', 'islamic knowledge', 2005, '2005-07-01 08:33:23', '2008-07-01 08:33:23');
 
 --
 -- Indexes for dumped tables
@@ -88,13 +108,30 @@ ALTER TABLE `authors`
 -- Indexes for table `author_book`
 --
 ALTER TABLE `author_book`
-  ADD KEY `foreignkey` (`author_id`,`book_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `foreign key` (`book_id`,`author_id`),
+  ADD KEY `book_id` (`book_id`),
+  ADD KEY `author_id` (`author_id`),
+  ADD KEY `book_id_2` (`book_id`),
+  ADD KEY `author_id_2` (`author_id`),
+  ADD KEY `author_id_3` (`author_id`);
 
 --
 -- Indexes for table `books`
 --
 ALTER TABLE `books`
   ADD PRIMARY KEY (`book_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `author_book`
+--
+ALTER TABLE `author_book`
+  ADD CONSTRAINT `author_book_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`),
+  ADD CONSTRAINT `author_book_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `authors` (`author_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
